7de0dc49c50090463e51ed6e41b3faca42fa9fc8
jvgoverna <jv11governa2018@gmail.com>
2022-10-18 22:31:09 -0300

    Esboço do projeto
    Inicialei o projeto de gerenciamento financeiro

---
+++ gerenciador.c
@@ -0,0 +1,77 @@
+
+#include <stdio.h>
+typedef struct{
+    char nome[50];
+    char sobrenome[50];
+    float valor;
+    char descrição[30];
+    char categoria[30];
+
+
+}gerenciador;
+
+void cadastrar(){
+    gerenciador g;
+    printf("Digite seu nome:\n");
+    scanf("%s",g.nome);
+    printf("Digite seu sobrenome:\n");
+    scanf("%s",g.sobrenome);
+    printf("Digite o valor que será colocado: ");
+    scanf("%f",&g.valor);
+    printf("Digite a descrição: ");
+    scanf("%s",g.descrição);
+    printf("Digite a categoria: ");
+    scanf("%s",g.categoria);
+
+}
+void arquivo_binário(){
+    gerenciador g;
+    FILE *arq = fopen("cadastro.bin","wb");
+    fwrite(&g,sizeof(gerenciador),1,arq);
+    fclose(arq);
+}
+
+void ler_binário(){
+    gerenciador g;
+    FILE *arq = fopen("cadastro.bin","rw");
+    fread(&g,sizeof(gerenciador),1,arq);
+    fclose(arq);
+    printf("Nome: %s\n",g.nome);
+    printf("Sobrenome: %s\n",g.sobrenome);
+    printf("Valor: %.2f\n",g.valor);
+    printf("Descrição: %s\n",g.descrição);
+    printf("Categoria: %s\n",g.categoria);
+}
+
+void passar_txt(){
+    gerenciador g;
+    FILE *arq = fopen("cadastro.txt","a");
+    fprintf(arq,"Nome: %s\n",g.nome);
+    fprintf(arq,"Sobrenome: %s\n",g.sobrenome);
+    fprintf(arq,"Valor: %.2f\n",g.valor);
+    fprintf(arq,"Descrição: %s\n",g.descrição);
+    fprintf(arq,"Categoria: %s\n",g.categoria);
+    fclose(arq);
+}
+
+
+
+int main(){
+    int n;
+    printf("----------Bem Vindo ao Gerenciador financeiro!!-----------\n");
+    printf("Digite a opção desejada: \n 1-cadastar\n");
+    scanf("%d",&n);
+
+    switch (n)
+    {
+    case 1:
+        cadastrar();
+        arquivo_binário();
+        ler_binário();
+        passar_txt();
+        break;
+    
+    default:
+        return n;
+    }
+}
